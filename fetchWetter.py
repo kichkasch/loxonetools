7045272ee9e025dac414f74e02696507676e0dcb
Michael Pilgermann <kichkasch@gmx.de>
2014-06-13 21:40:37 +0200

    added temperature to values and some comments

--- fetchWetter.py
+++ fetchWetter.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python
 # Python Skript zum Laden von Wetterdaten in die Loxone
 # Michael Pilgermann (kichkasch@gmx.de)
 # Letzte Aenderung: 2014-06-13
+#
+# Dieses Script muss auf einen Python-faehigen Rechner gespeichert und dort regelmaessig ausgefuehrt (CRON) werden (vorzugsweise also ein Server)
+# Als Croneintrag bietet sich an (entsprechende Lokation des Skriptes vorausgesetzt) - mit diesem Eintrag wird alle 10 Minuten aktualisiert:
+# */10 *  * * *	root	/usr/bin/fetchWetter.py
 
 URL="http://www.dwd.de/bvbw/appmanager/bvbw/dwdwwwDesktop?_nfpb=true&_pageLabel=_dwdwww_wetter_warnungen_regionenwetter&T1400077811143553394835gsbDocumentPath=Content%2FOeffentlichkeit%2FWV%2FWV11%2FWarnungen%2FWetter__Aktuell%2FRegionenwetter%2FRegion__Nordost__Teaser.html&_state=maximized&_windowLabel=T1400077811143553394835&lastPageLabel=_dwdwww_wetter_warnungen_regionenwetter"
 findBlockStr = "blockBodyPre"
@@ -12,6 +17,7 @@ LoxPrefix = "http://"
 LoxIP = "192.168.200.19"
 LoxPath = "/dev/sps/io"
 LoxVirtuellerEingang = "/WetterSonnig/"
+LoxVirtuellerEingangTemp = "/Aussentemperatur/"
 LoxUsername = "michael"
 LoxPassword = "*****"
 
@@ -31,12 +37,10 @@ targetLine = html[linePos:endPos]
 values = targetLine.split()
 temp = values[1]
 himmel = values[2]
-print himmel
 if himmel in himmelOK:
 	sonnig = "0"
 else:
 	sonnig = "1"
-
 
 # das Ganze nun auf der Loxone schalten
 url = LoxPrefix + LoxIP + LoxPath + LoxVirtuellerEingang + sonnig
@@ -45,3 +49,11 @@ base64string = base64.encodestring('%s:%s' % (LoxUsername, LoxPassword))[:-1]
 req.add_header("Authorization", "Basic %s" % base64string)
 handle = urllib2.urlopen(req)
 response = handle.read()
+
+url = LoxPrefix + LoxIP + LoxPath + LoxVirtuellerEingangTemp + temp 
+req = urllib2.Request(url)
+base64string = base64.encodestring('%s:%s' % (LoxUsername, LoxPassword))[:-1]
+req.add_header("Authorization", "Basic %s" % base64string)
+handle = urllib2.urlopen(req)
+response = handle.read()
+
